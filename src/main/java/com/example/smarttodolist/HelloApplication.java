5f4165ba89636a5d7be62e5593e2a916f245f103
Mohammad Afiq Harraz Bin Jumang Adha <afiqharrazz05@gmail.com>
2025-11-23 02:24:06 +0800

    Refactor HelloApplication to load To-Do List view

--- src/main/java/com/example/smarttodolist/HelloApplication.java
+++ src/main/java/com/example/smarttodolist/HelloApplication.java
@@ -1,19 +1,20 @@
-package com.example.smarttodolist;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+public class Main extends Application {
 
-public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/todo.fxml"));
+        primaryStage.setTitle("Smart To-Do List");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
     }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
